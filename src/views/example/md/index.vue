16acc850cdd356d65492de82062360f7b724e3ee
当时只道是寻常 <1207588603@qq.com>
2024-11-24 00:18:03 +0800

    feat: Markdown 编辑器组件

---
+++ src/views/example/md/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app-content">
+    <Markdown v-model="content" :height="500" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({ name: 'MdEditor' })
+
+const content = ref('**仰天大笑出门去，我辈岂是蓬蒿人。**')
+</script>
+
+<style lang="scss" scoped></style>
--- types/global-component.d.ts
+++ types/global-component.d.ts
@@ -3,7 +3,8 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     Icon: typeof import('../src/components/Icon/index.vue')['default']
-    AutoWrapList: typeof import('../src/components/AutoWrapList/index.vue')['default']
+    Markdown: typeof import('../src/components/Markdown/index.vue')['default']
     Screenfull: typeof import('../src/components/Screenfull/index.vue')['default']
+    AutoWrapList: typeof import('../src/components/AutoWrapList/index.vue')['default']
   }
 }
